da44d461f1b341fc15ecfc5ab3ce909245cbfb83
Adam Burt <adam.burt@etchuk.com>
2015-09-03 22:56:11 +0100

    Init commit

---
+++ config.rb
@@ -0,0 +1,12 @@
+project_type = :stand_alone
+
+preferred_syntax = :scss
+http_path = '/'
+css_dir = '/'
+sass_dir = '/'
+relative_assets = true
+line_comments = true
+sourcemap = true
+# output_style = :compressed
+
+require 'susy' 
