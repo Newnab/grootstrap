875f3462d33c1ac06bead4904ed41bf7bbc0c3d2
Adam Burt <adam.burt@etchuk.com>
2015-09-04 08:58:06 +0100

    Initial release version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # grootstrap
-I am Groot
+Did you know that Bootstrap features approximately zero Marvel movie references? That's just not acceptable in this day and age, is it? So I've clumsily shoehorned Vin Diesel's sensational work as a talking tree into a CSS stylesheet and made it into a thing you could theoretically actually use in production. For those of you who want to cover your markup in excessive, hard to read references to Guardians of the Galaxy, this is the grid framework for you.
+
+More docs to follow.
--- config.rb
+++ config.rb
@@ -10,3 +10,26 @@ sourcemap = true
 # output_style = :compressed
 
 require 'susy' 
+
+require 'fileutils'
+
+on_stylesheet_saved do |file|
+
+  if file.match('.min') == nil
+
+    require 'compass'
+
+    Compass.add_configuration(
+        {
+            :project_path => File.dirname(File.dirname(file)),
+            :sass_dir => File.basename(File.dirname(file)),
+            :css_path => File.basename(File.dirname(file)),
+            :output_style => :compressed
+        },
+        'alwaysmin' # A name for the configuration, can be anything you want
+    )
+    Compass.compiler.compile(File.dirname(file) + "/" + File.basename(file, '.css') + '.scss', File.dirname(file) + "/" + File.basename(file, '.css') + ".min.css")
+
+  end
+
+end
